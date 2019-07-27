d5adbbc4ba426534ed265a42beccbbb1c01c3114
曹桂凤 <demi.cao@oocl.com>
2019-07-27 16:19:39 +0800

    [Demi][feat:add CustomerOrder component]

--- src/router/router.js
+++ src/router/router.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import MobileHome from '../page/mobilePage/Home'
 import WebHome from '../page/webPage/Home'
+import CustomerOrder from '../component/CustomerOrder'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/mobile-home', name: 'mobile-home', component: MobileHome },
-  { path: '/web-home', name: 'web-home', component: WebHome }
+  { path: '/web-home', name: 'web-home', component: WebHome },
+  { path: '/customerOrder', name: 'customerOrder', component: CustomerOrder }
 ]
 
 const router = new VueRouter({
