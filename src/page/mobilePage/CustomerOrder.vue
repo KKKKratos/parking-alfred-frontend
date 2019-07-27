90e53d632beb85d675a1f7c73a98005996cf95e1
曹桂凤 <demi.cao@oocl.com>
2019-07-27 19:25:44 +0800

    [Demi][feat:fix ParkingBoyOrder which is CustomerOrders before]

--- src/page/mobilePage/CustomerOrder.vue
+++ src/page/mobilePage/CustomerOrder.vue
@@ -1,18 +1,27 @@
 <template>
     <div id ='item'>
-        <div id = 'customerOrders'>
-            <el-tag>粤C17278K(香洲区快乐停车场2号)&nbsp;8:00 </el-tag>
-            <el-button type="primary" size='mini'>待确认</el-button>
-            <br><br>
-            <el-tag type="info">粤C1752234K(香洲区快乐停车场)&nbsp;10:00</el-tag>
-            <el-tag type="info">已完成</el-tag>
+        <div id = 'parkingBoyOrders' style="text-align: left;">
+            <mt-cell title="粤C17278K" label="(香洲区快乐停车场2号)8:00" >
+                <el-button v-if="!isSure" type="primary" size='mini' @click="toMakeSure">待确认</el-button>
+                <img style='float:right' type="info" slot="icon" src="../../assets/img/tick.png" width="40" height="40" v-else/>
+            </mt-cell>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-  name: 'CustomerOrder'
+  name: 'parkingBoyOrders',
+   data() {
+       return {
+            isSure:false,
+       }
+   },
+   methods:{
+       toMakeSure:function(){
+           this.isSure=true;
+       }
+   }
 }
 </script>
 
