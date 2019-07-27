86111267006fe0d8aae9f2ea023023424ee62672
GalaxyXie <771472141@qq.com>
2019-07-27 16:25:46 +0800

    [Laura][feat: add clicked.png]

---
+++ src/component/ParkingLotItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+   <mt-cell title="停车场A(10)" v-model="clicked" ></mt-cell>
+  </div>
+</template>
+<script>
+export default {
+  name: "PrkingLotItem"
+};
+</script>
+<style scoped>
+@import "../assets/css/orderList.css";
+</style>
