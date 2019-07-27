aa122a6771fc1bc7aa08bbffbbc8f7c422e1de35
DALLIN GUO (ITA-ISDC-ISD-OOCLL/ZHA) <GUODA@OOCL.COM>
2019-07-27 16:00:28 +0800

    [dallin]:[feat:create_order_page]

--- src/router/router.js
+++ src/router/router.js
@@ -2,12 +2,16 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import MobileHome from '../page/mobilePage/Home'
 import WebHome from '../page/webPage/Home'
+import Order from '../components/Order'
+import ParkingBoyLogin from '../components/ParkingBoyLogin'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/mobile-home', name: 'mobile-home', component: MobileHome },
-  { path: '/web-home', name: 'web-home', component: WebHome }
+  { path: '/web-home', name: 'web-home', component: WebHome },
+  { path: '/order',name:'order',component:Order},
+  { path: '/parkingboylogin',name:'parkingboylogin',component:ParkingBoyLogin}
 ]
 
 const router = new VueRouter({
