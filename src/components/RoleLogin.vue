f9be8ca0fba6b9af50160fd28fda57611ba00633
DALLIN GUO (ITA-ISDC-ISD-OOCLL/ZHA) <GUODA@OOCL.COM>
2019-07-28 21:10:59 +0800

    [Dallin]:[feat]:finish_different_role_login

--- src/components/RoleLogin.vue
+++ src/components/RoleLogin.vue
@@ -21,7 +21,7 @@ export default {
     return {
       role: this.$route.params.selectedRole,
       labelPosition: "right",
-      formLabelAlign: {
+      formLabelAlign: {  
         name: "",
         password: ""
       }
