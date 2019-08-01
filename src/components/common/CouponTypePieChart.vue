477ce503116ca08427630620f42c641ee24bdc83
williamye <william.ye@oocl.com>
2019-08-01 16:06:34 +0800

    [william][feat: implement profit management]

---
+++ src/components/common/CouponTypePieChart.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="couponType" style="width: 600px;height:600px; position: absolute; margin-left: 600px;"></div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data: function () {
+    return {
+      touristAttractionName: [
+        '八折券', '满100减30', '买2送1'
+      ],
+      seriesData: [],
+      selectedName: {}
+    }
+  },
+  created () {
+    for (let i = 0; i < this.touristAttractionName.length; i++) {
+      this.seriesData.push({
+        name: this.touristAttractionName[i],
+        value: Math.round(Math.random() * 10000)
+      })
+      this.selectedName[this.touristAttractionName[i]] = i < 3
+    }
+  },
+  mounted () {
+    this.$echarts.init(document.getElementById('couponType')).setOption({
+      title: {
+        text: '各类型优惠券统计',
+        subtext: 'kratos',
+        x: 'center'
+      },
+      tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b} : {c} ({d}%)'
+      },
+      legend: {
+        type: 'scroll',
+        orient: 'vertical',
+        right: 10,
+        top: 20,
+        bottom: 20,
+        data: this.touristAttractionName,
+        selected: this.selectedName
+      },
+      series: {
+        name: '优惠券',
+        type: 'pie',
+        radius: '55%',
+        center: ['40%', '50%'],
+        data: this.seriesData,
+        itemStyle: {
+          emphasis: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    })
+    // myChart.setOption(this.option)
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/common/ProfitHistogram.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="profit" style="width: 1000px;height:300px;"></div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data: function () {
+    return {
+      seriesData: [],
+      colors: ['#5793f3', '#d14a61', '#675bba']
+    }
+  },
+  created () {
+    for (let i = 0; i < 10; i++) {
+      this.seriesData.push(parseInt(Math.round(Math.random() * 40000) + 10000))
+    }
+  },
+  mounted () {
+    this.$echarts.init(document.getElementById('profit')).setOption({
+      color: this.colors,
+      title: {
+        text: '当月各类型优惠券统计',
+        subtext: 'kratos',
+        x: 'center'
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'cross'
+        }
+      },
+      grid: {
+        right: '20%'
+      },
+      toolbox: {
+        feature: {
+          dataView: { show: true, readOnly: false },
+          restore: { show: true },
+          saveAsImage: { show: true }
+        }
+      },
+      xAxis: [
+        {
+          type: 'category',
+          name: '第X天',
+          axisTick: {
+            alignWithLabel: true
+          },
+          data: ['3', '6', '9', '12', '15', '18', '21', '24', '27', '30']
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value',
+          name: '盈利',
+          min: 0,
+          max: 60000,
+          position: 'left',
+          axisLine: {
+            lineStyle: {
+              color: this.colors[2]
+            }
+          },
+          axisLabel: {
+            formatter: '{value} 元'
+          }
+        }
+      ],
+      series: [
+        {
+          name: '盈利1',
+          type: 'bar',
+          data: this.seriesData
+        },
+        {
+          name: '盈利2  ',
+          type: 'line',
+          data: this.seriesData
+        }
+      ]
+    })
+    // myChart.setOption(this.option)
+  }
+}
+</script>
+
+<style scoped>
+</style>
